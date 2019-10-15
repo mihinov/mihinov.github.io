c1672027baf12785be2d27795f04a33fd8c690a3
Kamahin Mihail <45814283+mihinov@users.noreply.github.com>
2019-10-15 18:12:31 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,64 @@
+var reqAnimFrame = (function() {
+    return requestAnimationFrame       ||
+           mozRequestAnimationFrame    ||
+           webkitRequestAnimationFrame ||
+           oRequestAnimationFrame      ||
+           msRequestAnimationFrame     ||
+    function(callback) {
+        setTimeout(callback, 1000 / 60);
+    }
+})();
+var myReq = null;
+var circleStartBoolean = false;
+
 function setProgress(percent, selector) {
-  let circle = selector.querySelector('.progressbar__thumb');
-  let total = Math.PI * circle.r.baseVal.value;
-  circle.style.strokeDasharray = `${total*percent/100} ${total*(1-percent/100)*2}`;
-  selector.querySelector('text').innerHTML = '<tspan>' + percent.toFixed(0) + '</tspan>%';
+    let circle = selector.querySelector('.progressbar__thumb');
+    let total = Math.PI * circle.r.baseVal.value;
+    circle.style.strokeDasharray = `${total*percent/100} ${total*(1-percent/100)*2}`;
+    selector.querySelector('text').innerHTML = '<tspan>' + percent.toFixed(0) + '</tspan>%';
 }
 
 function circle(number, final, selector) {
 	let mainSelector = document.querySelector(selector);
-	let timer = setInterval(function () {
-		setProgress(number, mainSelector);
-		if (number == final) {
-			clearInterval(timer);
-		}
-		number += 1;
-	}, 50);
-    
+	function circleStep() {
+        myReq = reqAnimFrame(circleStep);
+        setProgress(number, mainSelector);
+        if (number == final) {
+            cancelAnimationFrame(myReq);
+        }
+        number++;
+    }
+    circleStep();
 }
 
-circle(0, 90, '.progress__container:nth-child(1)');
-circle(0, 80, '.progress__container:nth-child(2)');
-circle(0, 70, '.progress__container:nth-child(3)');
-circle(0, 90, '.progress__container:nth-child(4)');
-
-var upShot = document.getElementById('up__shot');
-
-upShot.addEventListener('click', () => {
-	window.scrollTo(0, 0);
+var burger = document.querySelector('.burger'); // BURGER
+var main__nav = document.querySelector('.main__nav');// MAIN__NAV
+var toggle = false;
+burger.addEventListener('click', () => {
+    if (toggle == 'toggle') {
+        toggle = false;
+        up__shot.style.display = 'block';
+        document.body.style.overflow = 'auto';
+    } else {
+        toggle = 'toggle';
+        up__shot.style.display = 'none';
+        document.body.style.overflow = 'hidden';
+    }
+    burger.classList.toggle('toggle');
+    main__nav.classList.toggle('toggle');
 });
 
+main__nav.addEventListener('click', (target) => {
+    if (toggle == 'toggle' && target.target.className != 'nav__link') {
+        toggle = false;
+        up__shot.style.display = 'block';
+        document.body.style.overflow = 'auto';
+        burger.classList.toggle('toggle');
+        main__nav.classList.toggle('toggle');
+    }
+});
+
+var up__shot = document.querySelector('.up__shot');
 var cardsCircle = document.querySelector('.cards__progress__bar');
 
 window.addEventListener('scroll', () => {
@@ -74,21 +105,27 @@ window.addEventListener('scroll', () => {
         };
 
         if (targetPosition.bottom > windowPosition.top &&
-            targetPosition.top < windowPosition.bottom &&
+            targetPosition.top + 50 < windowPosition.bottom &&
             targetPosition.right > windowPosition.left &&
             targetPosition.left < windowPosition.right) {
-            console.clear();
-            console.log('Вы видите элемент :)');
-            // сюда вставить код для анимации кругов
-        } else {
-            console.clear();
-        };
+            function circleStart() {
+                circle(-1, 90, '.progress__container:nth-child(1)');
+                circle(-1, 80, '.progress__container:nth-child(2)');
+                circle(-1, 70, '.progress__container:nth-child(3)');
+                circle(-1, 90, '.progress__container:nth-child(4)');
+            }
+            if (circleStartBoolean == false) {
+                circleStartBoolean = true;
+                circleStart();
+            }
+        }
     };
 
     scrollUp();
-    headerVisible();
     visibleCircle(cardsCircle);
-    
+    if (!toggle) {
+        headerVisible();
+    }
 
 });
 
